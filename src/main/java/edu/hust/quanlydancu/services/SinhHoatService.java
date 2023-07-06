783f240df85cde88b5b2e9b73623cde7ea487096
Kage <74716316+Kage1246@users.noreply.github.com>
2023-07-06 07:55:58 +0700

    update

--- src/main/java/edu/hust/quanlydancu/services/SinhHoatService.java
+++ src/main/java/edu/hust/quanlydancu/services/SinhHoatService.java
@@ -5,12 +5,16 @@
 package edu.hust.quanlydancu.services;
 
 import edu.hust.quanlydancu.dtos.SinhHoatDTO;
+import edu.hust.quanlydancu.dtos.ThamGiaDTO;
 import edu.hust.quanlydancu.entities.SinhHoat;
+import edu.hust.quanlydancu.entities.ThamGia;
 import edu.hust.quanlydancu.mapper.SinhHoatMapper;
+import edu.hust.quanlydancu.mapper.ThamGiaMapper;
 import edu.hust.quanlydancu.repositories.SinhHoatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -19,15 +23,34 @@ public class SinhHoatService {
     SinhHoatRepository sinhHoatRepository;
     @Autowired
     SinhHoatMapper sinhHoatMapper;
+    @Autowired
+    ThamGiaMapper thamGiaMapper;
     public SinhHoatDTO getById(Integer id) {
-        return sinhHoatMapper.toDto(sinhHoatRepository.getReferenceById(id));
+        SinhHoat sinhHoat = sinhHoatRepository.getReferenceById(id);
+        SinhHoatDTO sinhHoatDTO = new SinhHoatDTO();
+        sinhHoatDTO.setId(sinhHoat.getId());
+        sinhHoatDTO.setChuDe(sinhHoat.getChuDe());
+        sinhHoatDTO.setBatDau(sinhHoat.getBatDau());
+        sinhHoatDTO.setKetThuc(sinhHoat.getKetThuc());
+        sinhHoatDTO.setDiaDiem(sinhHoat.getDiaDiem());
+        List<ThamGiaDTO> list = new ArrayList<>();
+        for (ThamGia thamGia : sinhHoat.getThamGiasById()) {
+            list.add(thamGiaMapper.toDto(thamGia));
+        }
+        sinhHoatDTO.setThamGiasById(list);
+        return sinhHoatDTO;
     }
     public List<SinhHoatDTO> getAll() {
-        return sinhHoatMapper.toDto(sinhHoatRepository.findAll());
+        List<SinhHoatDTO> listDto = new ArrayList<>();
+        List<SinhHoat> listEntity = sinhHoatRepository.findAll();
+        for (SinhHoat sinhHoat : listEntity) {
+            listDto.add(getById(sinhHoat.getId()));
+        }
+        return listDto;
     }
-    public SinhHoatDTO createNew(SinhHoatDTO dto) {
+    public void createNew(SinhHoatDTO dto) {
         SinhHoat entity = sinhHoatRepository.save(sinhHoatMapper.toEntity(dto));
-        return sinhHoatMapper.toDto(entity);
+        sinhHoatMapper.toDto(entity);
     }
     public void updateById(SinhHoatDTO dto, Integer id) {
         SinhHoat entity = sinhHoatMapper.toEntity(dto);
